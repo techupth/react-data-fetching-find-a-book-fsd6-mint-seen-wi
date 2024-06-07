74f2f2d3f8a7c0d6646265772a87c2c4bdf0696d
wiKanok <kanokwan1401.ks@gmail.com>
2024-06-07 13:56:21 +0700

    feat: wi-react-data-fetching-find a books

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import axios from "axios";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [bookList, setBookList] = useState([]);
+
+  // console.log(bookList);
+
+  const findBooks = async (event) => {
+    let books = event.target.value;
+    if (books.length > 0) {
+      const resultBook = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${books}`
+      );
+      setBookList(resultBook.data.items);
+    } else {
+      setBookList([]);
+    }
+  };
+
+  return (
+    <div className="App">
+      {/* start coding here */}
+      <h1>Find a Book</h1>
+      <input id="book" name="book" type="text" onChange={findBooks} />
+      <ul>
+        {bookList?.map((item, index) => {
+          return <li key={index}>{item.volumeInfo.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
