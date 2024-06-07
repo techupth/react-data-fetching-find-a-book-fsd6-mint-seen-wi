0c677c3c460e3de6abb58de561a68e29469d0ac4
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-06-07 14:12:58 +0700

    feat: add book search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import "./App.css";
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+
+  const [collectInput, setInput] = useState("")
+  const [searchResult, setSearchResult] = useState([])
+
+  const getInputResult = (event) => {
+    setInput(event.target.value)
+    dataFetcher()
+  }
+
+  const dataFetcher = async () => {
+    const fetch = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${collectInput}`)
+    setSearchResult(fetch.data.items)
+  }
+
+  useEffect(() => {
+    dataFetcher()
+  },[])
+
+  return <div className="App">
+    <h1>Find a Book</h1>
+    <input type="text" onChange={getInputResult} />
+    <ul>
+      {
+        searchResult.map((item) => { 
+          if (collectInput !== "") {
+        return (
+          <li>{item.volumeInfo.title}</li>
+        )}
+      })}
+    </ul>
+  </div>;
 }
 
 export default App;
